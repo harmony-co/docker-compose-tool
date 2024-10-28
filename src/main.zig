3ce17af11fe66077018b75e406770f6eeda597bc
DidaS <didasoficial@gmail.com>
2024-10-28 07:49:14 +0000

    Refactor logger
    Now we adhere to the standard logger module from zig (std.log)

--- src/main.zig
+++ src/main.zig
@@ -1,35 +1,48 @@
 const std = @import("std");
 const log = @import("util/logging.zig");
 
-var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-
-const _program_flags = struct {
-    debug_mode: bool = false,
+pub const std_options = .{
+    .logFn = log.logMessage,
 };
-
-const flags = _program_flags{ .debug_mode = true };
 
 fn ask(comptime question: []const u8, abort_on_refuse: bool) bool {
     if (question.len == 0) {
-        log.err("No question provided", .{}, true, 1);
+        log.fatal("No question provided", .{}, null);
     }
 
+    const std_out = std.io.getStdOut();
     const reader_interface = std.io.getStdIn().reader();
     var answer_buffer: [4096]u8 = undefined;
 
-    std.debug.print("{s}{s}{s}", .{ log.ANSI.Yellow, question ++ "\n", log.ANSI.Reset });
-    std.debug.print("Continue? [y/N] ", .{});
+    std_out.lock(.none) catch |e| {
+        log.fatal("{!}", .{e}, null);
+        return false;
+    };
+    defer std_out.unlock();
+
+    const writer = std_out.writer();
+
+    writer.print("{s}{s}{s}", .{ log.ANSI.BoldYellow, question ++ "\n", log.ANSI.BoldColorReset }) catch |e| {
+        log.fatal("{!}", .{e}, null);
+        return false;
+    };
+
+    writer.print("Continue? [y/N] ", .{}) catch |e| {
+        log.fatal("{!}", .{e}, null);
+        return false;
+    };
+
     const answer = reader_interface.readUntilDelimiterOrEof(&answer_buffer, '\n') catch |e| {
-        log.err("{!}", .{e}, true, 1);
+        log.fatal("{!}", .{e}, null);
         return false;
     };
 
     if (answer) |response| {
-        if (std.mem.eql(u8, response, "y")) {
+        if (response[0] == 'y') {
             log.ok("Continuing...", .{});
         } else {
             if (abort_on_refuse) {
-                log.err("Aborting...", .{}, true, 1);
+                log.fatal("Aborting...", .{}, null);
             } else {
                 log.warn("Continuing...", .{});
                 return true;
@@ -40,9 +53,9 @@ fn ask(comptime question: []const u8, abort_on_refuse: bool) bool {
     return false;
 }
 
-fn getCwd() []u8 {
-    return std.fs.cwd().realpathAlloc(arena.allocator(), ".") catch |e| {
-        log.err("Failed to get current working directory: {!}", .{e}, true, 1);
+fn getCwd(allocator: std.mem.Allocator) []u8 {
+    return std.fs.cwd().realpathAlloc(allocator, ".") catch |e| {
+        log.fatal("Failed to get current working directory: {!}", .{e}, null);
         unreachable;
     };
 }
@@ -52,17 +65,19 @@ fn exec(command: []const []const u8) std.process.Child.RunError!std.process.Chil
         .allocator = std.heap.page_allocator,
         .argv = command,
     }) catch |e| {
-        log.err("Failed to execute command: {!}", .{e}, false, null);
+        log.err("Failed to execute command: {!}", .{e});
         return e;
     };
 }
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const args = try std.process.argsAlloc(std.heap.page_allocator);
     defer std.process.argsFree(std.heap.page_allocator, args);
 
-    log.ok("Current working directory: {s}", .{getCwd()});
+    const allocator = arena.allocator();
+    log.ok("Current working directory: {s}", .{getCwd(allocator)});
 
     log.ok("Args: {d}", .{args.len});
 
@@ -75,16 +90,14 @@ pub fn main() !void {
     const out2 = try exec(&args2);
     log.ok("Child process exited with out: {s}", .{out2.stdout});
 
-    if (flags.debug_mode) {
-        log.debug("This is a debug message.", .{});
-        log.debug("Debug messages should only be displayed when the {s}`flags.debug_mode`{s} is true", .{
-            log.ANSI.BoldUnderline, log.ANSI.Reset,
-        });
-    }
+    log.debug("This is a debug message.", .{});
+    log.debug("Debug messages should only be displayed when running in {s}'debug mode'{s}", .{
+        log.ANSI.BoldUnderline, log.ANSI.ResetBoldUnderline,
+    });
 
     log.ok("This is a success message. {s}", .{"Hello, World!"});
     log.info("This is a test message.", .{});
     log.warn("This is a warning message.", .{});
-    log.err("This is an error message without fatal flag", .{}, false, null);
-    log.err("This is an error message with fatal flag (130)", .{}, true, 130);
+    log.err("This is an error message without fatal flag", .{});
+    log.fatal("This is an error message with fatal flag (130)", .{}, 130);
 }
--- src/util/logging.zig
+++ src/util/logging.zig
@@ -1,51 +1,57 @@
 const std = @import("std");
 
 pub const ANSI = enum {
-    pub const Green = "\x1b[1;32m";
-    pub const Cyan = "\x1b[1;36m";
-    pub const Yellow = "\x1b[1;33m";
-    pub const Red = "\x1b[1;31m";
-    pub const Purple = "\x1b[1;35m";
-    pub const Reset = "\x1b[0m";
+    pub const BoldGreen = "\x1b[1;32m";
+    pub const BoldCyan = "\x1b[1;36m";
+    pub const BoldYellow = "\x1b[1;33m";
+    pub const BoldRed = "\x1b[1;31m";
+    pub const BoldPurple = "\x1b[1;35m";
+    pub const BoldColorReset = "\x1b[22;39m";
     pub const BoldUnderline = "\x1b[1;4m";
+    pub const ResetBoldUnderline = "\x1b[22;24m";
     pub const Italic = "\x1b[3m";
     pub const ItalicReset = "\x1b[23m";
 };
 
-pub fn logMessage(comptime level: []const u8, comptime color: []const u8, comptime fmt: []const u8, args: anytype) void {
-    var buffer: [256]u8 = undefined;
-    const message = std.fmt.bufPrint(&buffer, fmt, args) catch |e| {
-        switch (e) {
-            std.fmt.BufPrintError.NoSpaceLeft => err("Buffer too small", .{}, true, 1),
-        }
-        return;
-    };
-    std.debug.print("{s}[{s}] {s}{s}\n", .{ color, level, ANSI.Reset, message });
-}
+pub const ok = std.log.scoped(.ok).info;
+pub const info = std.log.info;
+pub const warn = std.log.warn;
+pub const debug = std.log.debug;
+pub const err = std.log.err;
 
-pub fn ok(comptime fmt: []const u8, args: anytype) void {
-    logMessage("OK", ANSI.Green, fmt, args);
-}
-
-pub fn info(comptime fmt: []const u8, args: anytype) void {
-    logMessage("INFO", ANSI.Cyan, fmt, args);
-}
-
-pub fn warn(comptime fmt: []const u8, args: anytype) void {
-    logMessage("WARN", ANSI.Yellow, fmt, args);
-}
-
-pub fn debug(comptime fmt: []const u8, args: anytype) void {
-    logMessage("DEBUG", ANSI.Purple, fmt, args);
-}
-
-pub fn err(comptime fmt: []const u8, args: anytype, comptime fatal: bool, comptime status: ?u8) void {
-    logMessage("ERROR", ANSI.Red, fmt, args);
-    if (fatal) {
-        if (status) |code| {
-            std.process.exit(code);
-        } else {
-            std.process.exit(1);
-        }
+pub fn fatal(comptime format: []const u8, args: anytype, comptime exit_code: ?u8) void {
+    std.log.scoped(.fatal).err(format, args);
+    if (comptime exit_code) |code| {
+        std.process.exit(code);
+    } else {
+        std.process.exit(1);
     }
+}
+
+pub fn logMessage(
+    comptime level: std.log.Level,
+    comptime scope: @Type(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const level_string = switch (level) {
+        .debug => ANSI.BoldPurple ++ "[DEBUG]" ++ ANSI.BoldColorReset,
+        .err => ANSI.BoldRed ++ "[ERROR]" ++ ANSI.BoldColorReset,
+        .warn => ANSI.BoldYellow ++ "[WARN]" ++ ANSI.BoldColorReset,
+        .info => ANSI.BoldCyan ++ "[INFO]" ++ ANSI.BoldColorReset,
+    };
+
+    const prefix = switch (scope) {
+        .ok => ANSI.BoldGreen ++ "[OK]" ++ ANSI.BoldColorReset,
+        .fatal => ANSI.BoldRed ++ "[FATAL]" ++ ANSI.BoldColorReset,
+        else => level_string,
+    };
+
+    const io = comptime if (level == .err or level == .debug) std.io.getStdErr() else std.io.getStdOut();
+
+    io.lock(.none) catch return;
+    defer io.unlock();
+
+    const writer = io.writer();
+    nosuspend writer.print(prefix ++ " " ++ format ++ "\n", args) catch return;
 }
